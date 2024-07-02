3e48079f4bc2930e6b89f7ec6e6984f4b2af8581
nachiadhi <adhi2712@gmail.com>
2024-07-02 17:33:10 +0530

    new

--- Generateimgs.py
+++ Generateimgs.py
@@ -3,4 +3,4 @@ output = so.simple_image_download
 search = ["tamilnadu_bus"] #enter the prompt as per the images you have to download 
 
 for s in search:
-    output().download(s,50) #quantity of the images needed.
+    output().download(s,20) #quantity of the images needed.
